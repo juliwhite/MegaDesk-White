6a94b15357148370eb75e9ed3cebb8a72fe6a531
Ian <Ian@GEOFF-LENOVO>
2023-05-10 13:38:31 -0600

    taskcompletedCreate dotnet-desktop.yml

--- MegaDesk-White/DisplayQuote.Designer.cs
+++ MegaDesk-White/DisplayQuote.Designer.cs
@@ -31,6 +31,9 @@
             this.buttonClose = new System.Windows.Forms.Button();
             this.setDate = new System.Windows.Forms.Label();
             this.labelDate = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // buttonClose
@@ -60,18 +63,37 @@
             this.labelDate.Size = new System.Drawing.Size(0, 39);
             this.labelDate.TabIndex = 2;
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(204)))), ((int)(((byte)(104)))));
+            this.panel1.Controls.Add(this.dateTimePicker1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1879, 100);
+            this.panel1.TabIndex = 3;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(1234, 25);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(571, 38);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
             // DisplayQuote
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Info;
-            this.ClientSize = new System.Drawing.Size(1536, 827);
+            this.ClientSize = new System.Drawing.Size(1879, 1020);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.labelDate);
             this.Controls.Add(this.setDate);
             this.Controls.Add(this.buttonClose);
             this.Name = "DisplayQuote";
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "Display Quote";
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -82,5 +104,7 @@
         private System.Windows.Forms.Button buttonClose;
         private System.Windows.Forms.Label setDate;
         private System.Windows.Forms.Label labelDate;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
     }
 }
